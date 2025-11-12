584750b5fd075af30951edc7efe1ab6c801278d5
MikkoTirronen <MikkoATirronen@gmail.com>
2025-11-12 13:03:37 +0100

    added booking

---
+++ Data/Entities/Booking.cs
@@ -0,0 +1,18 @@
+namespace HotelApi.Data.Entities;
+
+public class Booking
+{
+    public int Id { get; set; }
+    public int RoomId { get; set; }
+    public Room Room { get; set; } = null!;
+    public int CustomerId { get; set; }
+    public Customer Customer { get; set; } = null!;
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public int NumPersons { get; set; }
+    public int ExtraBedsCount { get; set; } = 0;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    public int? InvoiceId { get; set; }
+    public Invoice? Invoice { get; set; }
+}
