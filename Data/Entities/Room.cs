db9e73df1444b2c3a8833fc7766204470ad210cf
MikkoTirronen <MikkoATirronen@gmail.com>
2025-11-12 12:19:05 +0100

    Added Room

---
+++ Data/Entities/Room.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace HotelApi.Data.Entities;
+
+public class Room
+{
+    public int Id { get; set; }
+    [Required] public string Number { get; set; } = null!;
+    public RoomType Type { get; set; }
+    public int BaseCapacity { get; set; }
+    public int MaxExtraBeds { get; set; }
+    [Column(TypeName = "decimal(10,2)")] public decimal PricePerNight;
+    public string? Amenities { get; set; }
+    public bool Active { get; set; } = true;
+    public ICollection<Booking> Bookings { get; set; } = new List<Booking>();
+}
