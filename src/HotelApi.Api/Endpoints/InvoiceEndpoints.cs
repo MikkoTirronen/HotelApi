f57c0a5576c6e2efcf2c41c3e5417e6c92a0090d
Mikko Tirronen <mikkoatirronen@gmail.com>
2025-11-13 23:22:25 +0100

    added Invoice Endpoints

---
+++ src/HotelApi.Api/Endpoints/InvoiceEndpoints.cs
@@ -0,0 +1,28 @@
+using HotelApi.src.HotelApi.Core.Interfaces;
+using HotelApi.src.HotelApi.Domain.Entities;
+
+namespace HotelApi.src.HotelApi.Api.Endpoints;
+
+public static class InvoiceEndpoints
+{
+    public static void MapInvoiceEndpoints(this WebApplication app)
+    {
+        app.MapGet("/invoices", async (IInvoiceService service) =>
+        {
+            Results.Ok(await service.GetAllInvoicesAsync());
+        });
+
+        app.MapGet("/invoices/{id:int}", async (int id, IInvoiceService service) =>
+        {
+            var invoice = await service.GetInvoiceByIdAsync(id);
+            return invoice != null ? Results.Ok(invoice) : Results.NotFound();
+        });
+
+        app.MapGet("/invoices/booking/{bookingId:int}", async (int bookingId, IInvoiceService service) =>
+        {
+            var invoice = await service.GetInvoiceByBookingIdAsync(bookingId);
+            return invoice != null ? Results.Ok(invoice) : Results.NotFound();
+        });
+
+    }
+}
