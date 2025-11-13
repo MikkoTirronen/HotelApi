6f27d32b5a640f75c6d2230c4ecdddca0bc65d9a
Mikko Tirronen <mikkoatirronen@gmail.com>
2025-11-13 18:06:23 +0100

    added Generic Repository

---
+++ Data/Repos/GenericRepository.cs
@@ -0,0 +1,21 @@
+using HotelApi.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelApi.Data.Repos;
+
+public class GenericRepository<T> where T : class
+{
+    private readonly HotelDbContext _context;
+    private readonly DbSet<T> _dbSet;
+    public GenericRepository(HotelDbContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<T>();
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync() => await _dbSet.ToListAsync();
+    public async Task<T?> GetByIdAsync(int id) => await _dbSet.FindAsync(id);
+    public async Task AddAsync(T entity) => await _dbSet.AddAsync(entity);
+    public void Remove(T entity) => _dbSet.Remove(entity);
+    public async Task SaveAsync() => await _context.SaveChangesAsync();
+}
