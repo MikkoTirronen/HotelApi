216b11d970cb49a110ccdc71addace0ce4d1af90
Mikko Tirronen <mikkoatirronen@gmail.com>
2025-11-13 18:06:08 +0100

    added HotelDBContext

---
+++ Data/Contexts/HotelDbContext.cs
@@ -0,0 +1,33 @@
+using HotelApi.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelApi.Data.Contexts;
+
+public class HotelDbContext(DbContextOptions<HotelDbContext> options) : DbContext(options)
+{
+    public DbSet<Room> Rooms => Set<Room>();
+    public DbSet<Customer> Customers => Set<Customer>();
+    public DbSet<Booking> Bookings => Set<Booking>();
+    public DbSet<Invoice> Invoices => Set<Invoice>();
+    public DbSet<PaymentRecord> Payments => Set<PaymentRecord>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Room>().HasIndex(r => r.Number).IsUnique();
+        modelBuilder.Entity<Invoice>().HasIndex(i => i.BookingId).IsUnique();
+
+        modelBuilder.Entity<Booking>()
+            .HasOne(b => b.Customer)
+            .WithMany(c => c.Bookings)
+            .HasForeignKey(b => b.CustomerId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Booking>()
+            .HasOne(b => b.Room)
+            .WithMany(r => r.Bookings)
+            .HasForeignKey(b => b.RoomId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
