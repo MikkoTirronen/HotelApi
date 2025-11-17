a37780d2ea9a1aa31de9d091ac0cf8a0dd53f3de
Mikko Tirronen <mikkoatirronen@gmail.com>
2025-11-17 22:41:49 +0100

    added RoomId to booking

--- src/HotelApi.Domain/DTOs/BookingDto.cs
+++ src/HotelApi.Domain/DTOs/BookingDto.cs
@@ -5,10 +5,12 @@ namespace HotelApi.src.HotelApi.Domain.DTOs;
 public class BookingDto
 {
     public int Id { get; set; }
+    public int RoomId { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
     public int NumPersons { get; set; }
     public decimal TotalPrice { get; set; }
+    public RoomDto Room { get; set; } = null!;
     public InvoiceStatus Status { get; set; } = InvoiceStatus.Unpaid;
     public CustomerDto Customer { get; set; } = null!;
     public InvoiceDto Invoice { get; set; } = null!;
