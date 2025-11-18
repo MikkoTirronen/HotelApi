d5fce5237054c046f0fed46bc31a4156f372051e
Mikko Tirronen <mikkoatirronen@gmail.com>
2025-11-18 23:50:57 +0100

    added create booking with customer dto

---
+++ src/HotelApi.Domain/DTOs/CreateBookingWithCustomerDto.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelApi.src.HotelApi.Domain.DTOs;
+
+public class CreateBookingWithCustomerDto
+{
+    public int RoomId { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public int NumPersons { get; set; }
+
+    [Required] public string Name { get; set; } = null!;
+    [EmailAddress] public string Email { get; set; } = null!;
+    public string? Phone { get; set; }
+}
