c1d0d0c73be9fb3deded3493b5f23c038b38d926
Mikko Tirronen <mikkoatirronen@gmail.com>
2025-11-18 08:34:05 +0100

    added interface for customer repository

---
+++ src/HotelApi.Data/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,13 @@
+using HotelApi.src.HotelApi.Domain.Entities;
+
+namespace HotelApi.src.HotelApi.Data.Interfaces;
+
+public interface ICustomerRepository
+{
+    Task<List<Customer>> GetAllAsync();
+    Task<Customer?> GetByIdAsync(int id);
+    Task AddAsync(Customer customer);
+    void Update(Customer customer);
+    void Delete(Customer customer);
+    Task SaveAsync();
+}
