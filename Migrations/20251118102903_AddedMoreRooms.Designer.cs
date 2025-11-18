b4f499dc467ad36c48ef46da916706a2768ccb36
Mikko Tirronen <mikkoatirronen@gmail.com>
2025-11-18 23:47:18 +0100

    database changes

--- Migrations/20251118102903_AddedMoreRooms.Designer.cs
+++ Migrations/20251118102903_AddedMoreRooms.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using HotelApi.src.HotelApi.Data.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace HotelApi.Migrations
 {
     [DbContext(typeof(HotelDbContext))]
-    partial class HotelDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251118102903_AddedMoreRooms")]
+    partial class AddedMoreRooms
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -116,7 +119,6 @@ namespace HotelApi.Migrations
                         .HasColumnType("timestamp with time zone");
 
                     b.Property<string>("Email")
-                        .IsRequired()
                         .HasColumnType("text");
 
                     b.Property<string>("Name")
