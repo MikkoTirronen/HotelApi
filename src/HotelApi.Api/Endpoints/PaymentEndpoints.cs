fd4f13df08d795475f3e74d6aca14b745dd56b3d
Mikko Tirronen <mikkoatirronen@gmail.com>
2025-11-20 19:19:14 +0100

    changed payment endpoint

--- src/HotelApi.Api/Endpoints/PaymentEndpoints.cs
+++ src/HotelApi.Api/Endpoints/PaymentEndpoints.cs
@@ -1,4 +1,5 @@
 using HotelApi.src.HotelApi.Core.Interfaces;
+using HotelApi.src.HotelApi.Domain.DTOs;
 
 namespace HotelApi.src.HotelApi.Api.Endpoints;
 
@@ -7,14 +8,18 @@ public static class PaymentEndpoints
     public static void MapPaymentEndpoints(this WebApplication app)
     {
         app.MapPost("/payments", async (
-        int invoiceId,
-        decimal amount,
-        string? method,
-        IPaymentService paymentService
-    ) =>
-    {
-        var payment = await paymentService.RegisterPaymentAsync(invoiceId, amount, method);
-        return Results.Ok(payment);
-    });
+            RegisterPaymentDto request,
+            IPaymentService paymentService
+        ) =>
+        {
+            var payment = await paymentService.RegisterPaymentAsync(
+                request.InvoiceId,
+                request.Customer,
+                request.Amount,
+                request.Method
+            );
+
+            return Results.Ok(payment);
+        });
     }
 }
